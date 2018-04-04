d82bfd358aa06d1b6e4c1d2372cf70f16fa91189
Weeken Gao <weeken@koodar.com>
2018-04-04 17:56:41 +0800

    test admin auth

--- middlewares/createAdminToken.js
+++ middlewares/createAdminToken.js
@@ -3,6 +3,6 @@ const jwt = require('jsonwebtoken')
 module.exports = async admin => {
   let adminToken = {name: admin.name}
   let secret = admin._id.toString()
-  let token = await jwt.sign(adminToken, secret, { expiresIn: '5s' })
+  let token = await jwt.sign(adminToken, secret, { expiresIn: '12h' })
   return token
 }
